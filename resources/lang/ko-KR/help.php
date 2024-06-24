cbdec94d33e9e744356e49d88068c82805c20871
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 08:53:53 +0900

    Update help.php
    Translating some items into Korean

--- resources/lang/ko-KR/help.php
+++ resources/lang/ko-KR/help.php
@@ -15,21 +15,21 @@ return [
 
     'more_info_title' => '자세한 정보',
 
-    'audit_help' => 'Checking this box will edit the asset record to reflect this new location. Leaving it unchecked will simply note the location in the audit log.<br><br>Note that if this asset is checked out, it will not change the location of the person, asset or location it is checked out to.',
+    'audit_help' => '이 상자를 선택하면 이 새 위치를 반영하도록 자산 기록이 편집됩니다. 선택하지 않은 채로 두면 감사 로그에 위치가 기록됩니다.<br><br>이 자산이 체크아웃되어도 체크아웃된 사람, 자산 또는 위치의 위치는 변경되지 않습니다.',
 
     'assets' => '자산은 일련 번호나 자산 꼬리표로 추적되는 품목들입니다. 특정 품목의 상황을 파악하는 것이 더 높은 가치를 갖는 추세입니다.',
 
     'categories' => '분류는 품목들을 구성할 떄 유용합니다. 몇가지 예시 분류들로는 &quot;데스크탑&quot;,&quot;랩탑&quot;,&quot;휴대폰&quot;,&quot;타블렛&quot; 등이 있지만, 당신이 원하는 대로 분류들을 사용 할 수 있습니다.',
 
-    'accessories' => '부속품들이란 사용자들에게 지급은 하지만 일련 번호가 없는(또는 특별히 추적할 필요가 없는) 것을 뜻합니다. 예를 들면, 컴퓨터의 마우스나 키보드들입니다.',
+    'accessories' => '부속품이란 사용자들에게 지급은 하지만 일련 번호가 없는(또는 특별히 추적할 필요가 없는) 것을 뜻합니다. 예를 들면, 컴퓨터의 마우스나 키보드들입니다.',
 
     'companies'     => '회사들은 단순한 식별 항목으로 사용될 수 있고, 전사 지원이 관리 설정에서 활성화 된다면 자산, 사용자, 기타 등의 표시 제한에 사용 될 수 있습니다.',
 
-    'components'    => '부품들은 HDD, RAM 같은, 한개의 자산의 일부분입니다.',
+    'components'    => '구성품은 HDD, RAM 같은, 한개의 자산의 일부분입니다.',
 
     'consumables'   => '소모품은 시간이 지남에 따라 소진되어 구매해야 하는 것들 입니다. 예로, 프린터 잉크나 복사 용지가 있습니다.',
 
     'depreciations' => '가치가 하락하는 자산들을 직선법에 의한 감가상각 설정을 할 수 있습니다.',
     
-    'empty_file'    => 'The importer detects that this file is empty.'
+    'empty_file'    => '가져오기에서 빈 파일이 감지되었습니다.'
 ];
