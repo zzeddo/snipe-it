dfe23a874b95d525699d83d1052d0ad21dd2b648
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 12:36:10 +0900

    Update general.php
    Translating some items into Korean

--- resources/lang/ko-KR/admin/depreciations/general.php
+++ resources/lang/ko-KR/admin/depreciations/general.php
@@ -6,11 +6,11 @@ return [
     'asset_depreciations'  					=> '자산 감가 상각',
     'create'  					            => '감가 상각 생성',
     'depreciation_name'  					=> '감가 상각 명',
-    'depreciation_min'                      => 'Floor Value of Depreciation',
+    'depreciation_min'                      => '감가상각 하한액',
     'number_of_months'  					=> '개월 수',
     'update'  					            => '감가 상각 갱신',
     'depreciation_min'                      => '감가상각 완료 후 최소 가치',
-    'no_depreciations_warning'               => '<strong>Warning: </strong>
-                      You do not currently have any depreciations set up.
-                      Please set up at least one depreciation to view the depreciation report.',
+    'no_depreciations_warning'               => '<strong>경고: </strong>
+                      현재 설정된 감가상각이 없습니다..
+                      감가상각 보고서를 보려면 감가상각을 하나 이상 설정하세요.',
 ];
