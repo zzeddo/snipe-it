c34e62c2d63d8c5fb7244b5dd4fddcb3cd31e87e
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 17:48:56 +0900

    Update message.php
    Translating some items into Korean

--- resources/lang/ko-KR/admin/labels/message.php
+++ resources/lang/ko-KR/admin/labels/message.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'invalid_return_count' => 'Invalid count returned from :name. Expected :expected, got :actual.',
-    'invalid_return_type'  => 'Invalid type returned from :name. Expected :expected, got :actual.',
-    'invalid_return_value' => 'Invalid value returned from :name. Expected :expected, got :actual.',
+    'invalid_return_count' => ':name 에서 잘못된 개수가 반환되었습니다. 예상값 :expected, 반환값 :actual.',
+    'invalid_return_type'  => ':name 에서 잚못된 유형이 반환되었습니다. 예상값 :expected, 반환값 :actual.',
+    'invalid_return_value' => ':name 에서 잘못된 값이 반환되었습니다. 예상값 :expected, 반환값 :actual.',
 
-    'does_not_exist' => 'Label does not exist',
+    'does_not_exist' => '라벨이 존재하지 않습니다.',
     
 ];
