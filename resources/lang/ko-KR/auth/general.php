76e1da2f363f946b2ffdd95b5f9001d5d3ecafce
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 10:31:16 +0900

    Update general.php
    Translating some items into Korean

--- resources/lang/ko-KR/auth/general.php
+++ resources/lang/ko-KR/auth/general.php
@@ -4,16 +4,16 @@ return [
     'send_password_link'	        => '비밀번호 재설정 링크 보내기',
     'email_reset_password'			=> '이메일 비밀번호 재설정',
     'reset_password'			    => '비밀번호 재설정',
-    'saml_login'                    => 'Login via SAML',
+    'saml_login'                    => 'SAML을 통해 로그인',
     'login'                         => '로그인',
     'login_prompt'                  => '로그인해주십시오',
     'forgot_password'               => '비밀번호 분실했습니다',
-    'ldap_reset_password'           => 'Please click here to reset your LDAP password',
+    'ldap_reset_password'           => 'LDAP 비밀번호를 재설정하려면 여기를 클릭하세요.',
     'remember_me'                   => '자동 로그인',
-    'username_help_top'                 => 'Enter your <strong>username</strong> to be emailed a password reset link.',
+    'username_help_top'                 => '비밀번호 재설정 링크를 이메일로 받으려면 <strong>사용자이름</strong>을 입력하세요.',
     'username_help_bottom'          => '사용자 계정과  이메일 주소는 구성에 따라 동일할 수 있지만 동일하지 않을 수 있습니다. 사용자 계정을 기억할 수 없는 경우 관리자에게 문의하십시오. 연결된 이메일 주소가 없는 사용자 계정은 암호 재설정 링크가 이메일로 전송되지 않습니다.',
-    'google_login'                  => 'Login with Google Workspace',
-    'google_login_failed'           => 'Google Login failed, please try again.',
+    'google_login'                  => 'Google Workspace로 로그인',
+    'google_login_failed'           => 'Google 로그인에 실패했습니다. 다시 시도해 주세요.',
 
 ];
 
