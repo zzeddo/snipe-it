21584dc92f48c4b001653ae646382a149c507556
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 09:13:22 +0900

    Update table.php
    Translating some items into Korean

--- resources/lang/ko-KR/table.php
+++ resources/lang/ko-KR/table.php
@@ -6,6 +6,6 @@ return array(
     'action' 		        => '작업',
     'by'      		        => '요청자',
     'item' 			        => '항목',
-    'no_matching_records'   => 'No matching records found',
+    'no_matching_records'   => '일치하는 대상이 없습니다.',
 
 );
