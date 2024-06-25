3b6a0a01b96b2bd6bc52b598b88bc53cf356f99f
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 16:50:07 +0900

    Update message.php
    Translating some items into Korean

--- resources/lang/ko-KR/admin/hardware/message.php
+++ resources/lang/ko-KR/admin/hardware/message.php
@@ -3,38 +3,38 @@
 return [
 
     'undeployable' 		=> '<strong>경고: </strong> 이 자산은 현재 사용불가 상태입니다.
-                        이 상태를 변경하시려면, 자산 상태를 갱신하세요.',
+                        이 상태를 변경하시려면, 자산 상태를 수정하세요.',
     'does_not_exist' 	=> '자산이 존재하지 않습니다.',
-    'does_not_exist_var'=> 'Asset with tag :asset_tag not found.',
-    'no_tag' 	        => 'No asset tag provided.',
-    'does_not_exist_or_not_requestable' => 'That asset does not exist or is not requestable.',
+    'does_not_exist_var'=> '자산 테그 :asset_tag 존재하지 않습니다.',
+    'no_tag' 	        => '자산 태그가 없습니다.',
+    'does_not_exist_or_not_requestable' => '해당 자산이 존재하지 않거나 요청할 수 없습니다.',
     'assoc_users'	 	=> '이 자산은 현재 사용자에게 반출 중이어서 삭제 할 수 없습니다. 먼저 자산을 확인해 보고 다시 삭제를 시도해 주세요. ',
-    'warning_audit_date_mismatch' 	=> 'This asset\'s next audit date (:next_audit_date) is before the last audit date (:last_audit_date). Please update the next audit date.',
+    'warning_audit_date_mismatch' 	=> '이 자산의 다음 감사일(:next_audit_date)는 마지막 감사일(:last_audit_date)보다 이전입니다. 다음 감사일을 변경하세요',
 
     'create' => [
         'error'   		=> '자산이 생성되지 않았습니다. 다시 시도해 주세요. :(',
         'success' 		=> '자산이 생성되었습니다. :)',
-        'success_linked' => 'Asset with tag :tag was created successfully. <strong><a href=":link" style="color: white;">Click here to view</a></strong>.',
+        'success_linked' => '자산 태그 :tag was 생성되었습니다.. <strong><a href=":link" style="color: white;">보기를 클릭하세요</a></strong>.',
     ],
 
     'update' => [
-        'error'   			=> '자산이 갱신되지 않았습니다. 다시 시도해 주세요.',
-        'success' 			=> '자산이 갱신되었습니다.',
-        'encrypted_warning' => 'Asset updated successfully, but encrypted custom fields were not due to permissions',
-        'nothing_updated'	=>  '선택된 항목이 없어서, 갱신 되지 않습니다.',
-        'no_assets_selected'  =>  'No assets were selected, so nothing was updated.',
-        'assets_do_not_exist_or_are_invalid' => 'Selected assets cannot be updated.',
+        'error'   			=> '자산이 수정되지 않았습니다. 다시 시도해 주세요.',
+        'success' 			=> '자산이 수정되었습니다.',
+        'encrypted_warning' => '자산이 성공적으로 변경하였지만, 암호화된 사용자 필드는 권한으로 인해 발생하지 않았습니다.',
+        'nothing_updated'	=>  '선택된 항목이 없어서, 수정 되지 않습니다.',
+        'no_assets_selected'  =>  '선택된 자산이 없어 수정되지 않았습니다.',
+        'assets_do_not_exist_or_are_invalid' => '선택된 자산은 수정할 수 없습니다.',
     ],
 
     'restore' => [
         'error'   		=> '자산이 복원되지 않았습니다. 다시 시도해 주세요.',
         'success' 		=> '자산이 복원되었습니다.',
         'bulk_success' 		=> '자산이 복원되었습니다.',
-        'nothing_updated'   => 'No assets were selected, so nothing was restored.', 
+        'nothing_updated'   => '선택된 자산이 없어 복원되지 않았습니다.', 
     ],
 
     'audit' => [
-        'error'   		=> 'Asset audit unsuccessful: :error ',
+        'error'   		=> '자산 감사 실패: :error ',
         'success' 		=> '자산 감사가 성공적으로 기록되었습니다.',
     ],
 
@@ -57,9 +57,9 @@ return [
         'success'               => '파일에서 읽어오기가 완료되었습니다',
         'file_delete_success'   => '파일 삭제가 완료되었습니다',
         'file_delete_error'      => '파일을 삭제할 수 없습니다',
-        'file_missing' => 'The file selected is missing',
-        'header_row_has_malformed_characters' => 'One or more attributes in the header row contain malformed UTF-8 characters',
-        'content_row_has_malformed_characters' => 'One or more attributes in the first row of content contain malformed UTF-8 characters',
+        'file_missing' => '선택한 파일이 없습니다.',
+        'header_row_has_malformed_characters' => '헤더 행에 있는 하나 이상의 속성에 잘못된 형식의 UTF-8 문자가 포함되어 있습니다.',
+        'content_row_has_malformed_characters' => '콘텐츠의 첫 번째 행에 있는 하나 이상의 속성에 잘못된 형식의 UTF-8 문자가 포함되어 있습니다.',
     ],
 
 
