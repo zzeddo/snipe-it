10ce75b4b94b4054176e1a39c81669de8c949a4e
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 11:38:39 +0900

    Update table.php
    Translating some items into Korean

--- resources/lang/ko-KR/admin/companies/table.php
+++ resources/lang/ko-KR/admin/companies/table.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-    'companies' => '회사들',
+    'companies' => '회사',
     'create'    => '회사 생성',
-    'email'     => 'Company Email',
+    'email'     => '회사 이메일',
     'title'     => '회사',
-    'phone'     => 'Company Phone',
-    'update'    => '회사 갱신',
+    'phone'     => '대표 번호',
+    'update'    => '회사 수정',
     'name'      => '회사명',
     'id'        => '아이디',
 );
