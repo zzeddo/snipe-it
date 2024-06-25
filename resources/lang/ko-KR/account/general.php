796d921f6b8032e9d6446cb4769e25162e8eea07
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 10:40:10 +0900

    Update general.php
    Translating some items into Korean

--- resources/lang/ko-KR/account/general.php
+++ resources/lang/ko-KR/account/general.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-    'personal_api_keys' => 'Personal API Keys',
-    'api_key_warning' => 'When generating an API token, be sure to copy it down immediately as they will not be visible to you again.',
-    'api_base_url' => 'Your API base url is located at:',
+    'personal_api_keys' => '개인 API 키',
+    'api_key_warning' => 'API 토큰 생성시 다시 표시되지 않으므로 복사해 두십시오.',
+    'api_base_url' => 'API 기본 URL은 다음 위치에 있습니다:',
     'api_base_url_endpoint' => '/&lt;endpoint&gt;',
-    'api_token_expiration_time' => 'API tokens are set to expire in:',
-    'api_reference' => 'Please check the <a href="https://snipe-it.readme.io/reference" target="_blank">API reference</a> to find specific API endpoints and additional API documentation.',
-    'profile_updated' => 'Account successfully updated',
+    'api_token_expiration_time' => 'API 토큰은 다음 날짜에 만료됩니다:',
+    'api_reference' => '특정 API 엔드포인트와 추가 API 문서를 찾으려면 <a href="https://snipe-it.readme.io/reference" target="_blank">API 참조</a> 를 확인하세요',
+    'profile_updated' => '계정이 업데이트되었습니다.',
 );
