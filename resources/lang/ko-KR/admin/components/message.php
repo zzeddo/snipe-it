7eebe0fecd3a6244a78aa4db25150754475b3b6e
PARK JAEDO <zzeddo@gmail.com>
2024-06-25 11:42:55 +0900

    Update message.php
    Translating some items into Korean

--- resources/lang/ko-KR/admin/components/message.php
+++ resources/lang/ko-KR/admin/components/message.php
@@ -2,34 +2,34 @@
 
 return array(
 
-    'does_not_exist' => '부품이 존재하지 않습니다.',
+    'does_not_exist' => '구성품이 존재하지 않습니다.',
 
     'create' => array(
-        'error'   => '부품이 생성되지 않았습니다. 다시 시도해 주세요.',
-        'success' => '부품이 생성되었습니다.'
+        'error'   => '구성품이 생성되지 않았습니다. 다시 시도해 주세요.',
+        'success' => '구성품이 생성되었습니다.'
     ),
 
     'update' => array(
-        'error'   => '부품이 갱신되지 않았습니다. 다시 시도해 주세요.',
-        'success' => '부품이 갱신 되었습니다.'
+        'error'   => '구성품이 갱신되지 않았습니다. 다시 시도해 주세요.',
+        'success' => '구성품이 갱신 되었습니다.'
     ),
 
     'delete' => array(
-        'confirm'   => '이 부품을 삭제하시겠습니까?',
-        'error'   => '부품 삭제시 문제가 발생했습니다. 다시 시도해 주세요.',
-        'success' => '부품이 삭제되었습니다.'
+        'confirm'   => '이 구성품을 삭제하시겠습니까?',
+        'error'   => '구성품 삭제시 문제가 발생했습니다. 다시 시도해 주세요.',
+        'success' => '구성품이 삭제되었습니다.'
     ),
 
      'checkout' => array(
-        'error'   		=> '부품이 반출되지 않았습니다. 다시 시도해 주세요.',
-        'success' 		=> '부품이 반출 되었습니다.',
+        'error'   		=> '구성품이 반출되지 않았습니다. 다시 시도해 주세요.',
+        'success' 		=> '구성품이 반출 되었습니다.',
         'user_does_not_exist' => '잘못된 사용자 입니다. 다시 시도해 주세요.',
-        'unavailable'      => 'Not enough components remaining: :remaining remaining, :requested requested ',
+        'unavailable'      => '남은 구성품이 충분하지 않음: :remaining 남음, :requested 요청됨 ',
     ),
 
     'checkin' => array(
-        'error'   		=> '부품이 반입되지 않았습니다. 다시 시도해 주세요.',
-        'success' 		=> '부품이 반입되었습니다.',
+        'error'   		=> '구성품이 반입되지 않았습니다. 다시 시도해 주세요.',
+        'success' 		=> '구성품이 반입되었습니다.',
         'user_does_not_exist' => '잘못된 사용자 입니다. 다시 시도해 주세요.'
     )
 
