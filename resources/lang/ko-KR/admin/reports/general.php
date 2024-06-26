005a567ab298f8f20ee5de770794e837c90cf913
PARK JAEDO <zzeddo@gmail.com>
2024-06-26 09:17:51 +0900

    Update general.php
    Translating some items into Korean

--- resources/lang/ko-KR/admin/reports/general.php
+++ resources/lang/ko-KR/admin/reports/general.php
@@ -2,16 +2,16 @@
 
 return [
     'info'   => '자산 보고서의 옵션을 선택해 주세요.',
-    'deleted_user' => 'Deleted user',
-    'send_reminder' => 'Send reminder',
-    'reminder_sent' => 'Reminder sent',
-    'acceptance_deleted' => 'Acceptance request deleted',
-    'acceptance_request' => 'Acceptance request',
+    'deleted_user' => '삭제된 사용자',
+    'send_reminder' => '알람 보내기',
+    'reminder_sent' => '알람 전송됨',
+    'acceptance_deleted' => '수락 요청 삭제',
+    'acceptance_request' => '수락 요청',
     'custom_export' => [
-        'user_address' => 'User Address',
-        'user_city' => 'User City',
-        'user_state' => 'User State',
-        'user_country' => 'User Country',
-        'user_zip' => 'User Zip'
+        'user_address' => '주소',
+        'user_city' => '시/도',
+        'user_state' => '시/군/구',
+        'user_country' => '국가',
+        'user_zip' => '우편번호'
     ]
 ];
