ff8d657b9812ea7ad086ed1bfce5135e7cf8b1c5
PARK JAEDO <zzeddo@gmail.com>
2024-06-26 17:10:45 +0900

    Update table.php

--- resources/lang/ko-KR/admin/manufacturers/table.php
+++ resources/lang/ko-KR/admin/manufacturers/table.php
@@ -2,7 +2,7 @@
 
 return array(
     'about_manufacturers_title'    => '제조업체란',
-    'about_manufacturers_text'  => '생산자는 당신의 자산들을 생산한 회사들입니다. 여기에 자산 상세 페이지에 나타날, 그들에 대한 중요한 지원 연락처 정보를 저장 할 수 있습니다.',
+    'about_manufacturers_text'  => '생산자는 당신의 자산들을 생산한 회사입니다. 여기에 자산 상세 페이지에 나타날, 그들에 대한 중요한 지원 연락처 정보를 저장 할 수 있습니다.',
     'asset_manufacturers'	=> '자산 제조업체',
     'create'				=> '제조업체 생성',
     'id'   					=> '아이디',
