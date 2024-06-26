cea7ea16b211feb456e68b6513a6d27930ab9537
PARK JAEDO <zzeddo@gmail.com>
2024-06-26 12:29:47 +0900

    Update table.php

--- resources/lang/ko-KR/admin/accessories/table.php
+++ resources/lang/ko-KR/admin/accessories/table.php
@@ -5,7 +5,7 @@ return array(
 	'eula_text'      			=> '사용권 계약서',
     'id'      					=> '아이디',
     'require_acceptance'      	=> '승인',
-    'title'      				=> '부속품 명',
+    'title'      				=> '부속품명',
 
 
 );
