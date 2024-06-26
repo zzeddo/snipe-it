a2830f9037fdaa11c1b3c353eb3aa2bc8080159c
PARK JAEDO <zzeddo@gmail.com>
2024-06-26 12:23:48 +0900

    Update general.php

--- resources/lang/ko-KR/auth/general.php
+++ resources/lang/ko-KR/auth/general.php
@@ -12,7 +12,7 @@ return [
     'remember_me'                   => '자동 로그인',
     'username_help_top'                 => '비밀번호 재설정 링크를 이메일로 받으려면 <strong>사용자이름</strong>을 입력하세요.',
     'username_help_bottom'          => '사용자 계정과  이메일 주소는 구성에 따라 동일할 수 있지만 동일하지 않을 수 있습니다. 사용자 계정을 기억할 수 없는 경우 관리자에게 문의하십시오. 연결된 이메일 주소가 없는 사용자 계정은 암호 재설정 링크가 이메일로 전송되지 않습니다.',
-    'google_login'                  => 'Google Workspace로 로그인',
+    'google_login'                  => 'Google 워크스페이스 로그인',
     'google_login_failed'           => 'Google 로그인에 실패했습니다. 다시 시도해 주세요.',
 
 ];
